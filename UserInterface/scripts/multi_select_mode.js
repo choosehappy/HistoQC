dbfbc1a980d721d4ad2b9e9fd88da914e2aeef0d
Ren Zuo <renzuo.wren@gmail.com>
2020-06-26 21:19:22 -0400

    Start individual script for multi select mdoe

---
+++ UserInterface/scripts/multi_select_mode.js
@@ -0,0 +1,38 @@
+function update_multi_selected () {
+	CURRENT_MULTI_SELECTED = ORIGINAL_DATASET.filter(function (d) {
+		if (
+			(PARA_COOR_SELECTED.length == 0 || PARA_COOR_SELECTED.indexOf(d["filename"]) != -1) && 
+			(UMAP_PROJ_SELECTED.length == 0 || UMAP_PROJ_SELECTED.indexOf(d["filename"]) != -1)
+		) {
+			return true;
+		}
+		return false;
+	})
+	CURRENT_CASE_LIST = CURRENT_MULTI_SELECTED.map(function(d){return d["filename"];});
+
+	update_multi_selected_views(CURRENT_CASE_LIST);
+}
+
+
+function update_multi_selected_views (file_names) {
+	update_multi_selected_chart_view(file_names);
+	update_multi_selected_image_view(file_names);
+	update_multi_selected_table_view();
+}
+
+
+// function update_multi_selected (file_names) {
+// 	// in parallel coordinate, user could brush to filter to a subset of cases.  Other views
+// 	// should respond to only display the filtered cases.
+
+// 	CURRENT_MULTI_SELECTED = ORIGINAL_DATASET.filter(function (d) {
+// 		if (file_names.indexOf(d["filename"]) != -1) {
+// 			return true;
+// 		}
+// 		return false;
+// 	})
+// 	CURRENT_CASE_LIST = CURRENT_MULTI_SELECTED.map(function(d){return d["filename"];});
+// 	update_multi_selected_chart_view();
+// 	update_multi_selected_image_view(file_names);
+// 	update_multi_selected_table_view();
+// }
