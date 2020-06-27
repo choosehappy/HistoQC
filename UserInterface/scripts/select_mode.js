ae2780db3289434ff3153ebc2ac780afbfbe49c4
Ren Zuo <renzuo.wren@gmail.com>
2020-06-26 21:25:23 -0400

    removed multi select mode related code

--- UserInterface/scripts/select_mode.js
+++ UserInterface/scripts/select_mode.js
@@ -27,19 +27,4 @@ function update_views () {
 }
 
 
-function update_multi_selected (file_names) {
-	// in parallel coordinate, user could brush to filter to a subset of cases.  Other views
-	// should respond to only display the filtered cases.
-
-	CURRENT_MULTI_SELECTED = ORIGINAL_DATASET.filter(function (d) {
-		if (file_names.indexOf(d["filename"]) != -1) {
-			return true;
-		}
-		return false;
-	})
-	CURRENT_CASE_LIST = CURRENT_MULTI_SELECTED.map(function(d){return d["filename"];});
-	update_multi_selected_chart_view();
-	update_multi_selected_image_view(file_names);
-	update_multi_selected_table_view();
-}
 
