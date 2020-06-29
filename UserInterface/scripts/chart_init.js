fb2aaf55205933b86f4c573f94ce8de4a3548f0a
Ren Zuo <renzuo.wren@gmail.com>
2020-06-29 02:34:40 -0400

    Removed chart specific logic and moved them to chart specific scripts.

--- UserInterface/scripts/chart_init.js
+++ UserInterface/scripts/chart_init.js
@@ -9,43 +9,17 @@ function initialize_chart_view (dataset, vis_type="bar_chart") {
 	show_view("chart");
 	update_chart_width();
 
-	$CHART.empty();
-	$PARAC.empty();
-	$DRPLT.empty();
-
-	// init global SVG and MARGIN
-	CHART_MARGIN = {top: 10, right: 60, bottom: 40, left: 10};
-	if (dataset.length > 50) {
-		CHART_MARGIN.bottom = 10;
-	}
-	PARAC_MARGIN = {top: 80, right: 40, bottom: 10, left: 10};
-	DRPLT_MARGIN = {top: 10, right: 10, bottom: 30, left: 30};
-
-	CHART_SVG = d3.select("#chart-svg-container").append("svg")
-		.attr("id", "chart-svg")
-		.attr("width", $CHART.width())
-		.attr("height", $CHART.height())
-		.append("g")
-		.attr("transform", "translate(" + CHART_MARGIN.left + "," + CHART_MARGIN.top + ")");
-	PARAC_SVG = d3.select("#parac-svg-container").append("svg")
-		.attr("id", "parac-svg")
-		.attr("width", $PARAC.width())
-		.attr("height", $PARAC.height())
-		.append("g")
-		.attr("transform", "translate(" + PARAC_MARGIN.left + "," + PARAC_MARGIN.top + ")");
-	DRPLT_SVG = d3.select("#drplt-svg-container").append("svg")
-		.attr("id", "drplt-svg")
-		.attr("width", $DRPLT.width())
-		.attr("height", $DRPLT.height())
-		.append("g")
-		.attr("transform", "translate(" + DRPLT_MARGIN.left + "," + DRPLT_MARGIN.top + ")");
+	init_bar_canvas();
+	init_parallel_canvas();
+	init_scatter_canvas();
 
 	CURRENT_CHART_ATTRIBUTE = DEFAULT_CHART_ATTRIBUTE;
 
 	init_bar_chart(dataset);
 	init_parallel_coordinate(dataset);
 	init_scatter_plot(dataset);
-	init_chart_selector(vis_type);
+
+	init_chart_selector();
 
 	show_chosen_vis(vis_type);
 }
@@ -143,31 +117,11 @@ function update_multi_selected_chart_view (selected_cases) {
 }
 
 
-function init_chart_selector (vis_type) {
+function init_chart_selector () {
 
-	// TODO: clean up useless code
-	$chart_selector = $("#chart-select");
-	$chart_selector.empty();
-
-	var sample_case = ORIGINAL_DATASET[0];
-
-	for (var index in Object.keys(sample_case)) {
-		var key = Object.keys(sample_case)[index];
-		if (typeof(sample_case[key]) == "number") {
-			if (key == CURRENT_CHART_ATTRIBUTE) {
-				$chart_selector.append(generate_option_html(key, key, true));
-			} else {
-				$chart_selector.append(generate_option_html(key, key));
-			}
-		}
-	}
-	$('.selectpicker').selectpicker('render');
-	$('.selectpicker').selectpicker('refresh');
-
-	$chart_selector.change(function () {
-		CURRENT_CHART_ATTRIBUTE = $(this).val();
-		update_chart_view("bar_chart", CURRENT_MULTI_SELECTED);
-	});
+	init_bar_vars_selector();
+	init_parallel_vars_selector();
+	init_scatter_vars_selector();
 
 	$("#vis-switch-btn").click(function () {
 		if ($PARAC.css("display") == "none") {
@@ -184,28 +138,29 @@ function init_chart_selector (vis_type) {
 		data_sorting(sort_attribute, true);
 		update_views();
 	})
-
-	function generate_option_html (key, value, selected = false) {
-		if (selected) {
-			return "<option value='" + value + "' selected>" + key + "</option>";
-		} else {
-			return "<option value='" + value + "'>" + key + "</option>";
-		}
-	}
 }
+
 
 function show_chosen_vis (vis_type) {
 	if (vis_type == "bar_chart") {
 		CURRENT_VIS_TYPE = "bar_chart";
+
+		$CHART.css("display", "");
+		$("#bar-select-group").css("display", "");
+		$("#chart-sort-btn").css("display", "");
 		$PARAC.css("display", "none");
-		$CHART.css("display", "block");
-		$("#chart-select-group").css("display", "flex");
+		$("#parallel-select-group").css("display", "none");
+
 		$("#vis-switch-btn").text("Parallel Coordinate");
 	} else if (vis_type == "parallel_coordinate") {
 		CURRENT_VIS_TYPE = "parallel_coordinate";
+
 		$CHART.css("display", "none");
-		$("#chart-select-group").css("display", "none");
-		$PARAC.css("display", "block");
+		$("#bar-select-group").css("display", "none");
+		$("#chart-sort-btn").css("display", "none");
+		$PARAC.css("display", "");
+		$("#parallel-select-group").css("display", "");
+
 		$("#vis-switch-btn").text("Bar Chart");
 	} 
 }
