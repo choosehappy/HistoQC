12f7e263006fcb79c720aaf42fccbb839cce5701
jackson <jacksonjjacobs@gmail.com>
2023-08-21 15:02:09 -0400

    remove breakpoint

--- histoqc/_worker.py
+++ histoqc/_worker.py
@@ -54,7 +54,6 @@ def worker(idx, file_name, process_queue, config, outdir, num_files, force, shar
     logging.info(f"-----Working on:\t{file_name}\t\t{idx+1} of {num_files}")
 
     try:
-        breakpoint()
         s = BaseImage(file_name, fname_outdir, dict(config.items("BaseImage.BaseImage")))
         for process, process_params in process_queue:
             #process_params["lock"] = lock
